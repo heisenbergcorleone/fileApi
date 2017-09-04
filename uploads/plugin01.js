32b02fc600cf72be3a15fead02fb3eab1aa9a9c0
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-09-04 15:30:53 +0530

    plugin deleted

--- uploads/plugin01.js
+++
@@ -1,9 +0,0 @@
-var num = document.getElementById("number")
-function triple (x) {
-    console.log(x*3)
-}
-num.addEventListener("keyup", function(e) {
-    if(e.keyCode == 13) {
-       triple(num.value);
-    }
-})
